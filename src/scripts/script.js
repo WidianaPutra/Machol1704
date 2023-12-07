1a6558c5845652642fcd9c5b0dd891878a6d7142
WidianaPutra <olenggamer@gmail.com>
2023-12-07 14:08:17 +0800

    upload

---
+++ src/scripts/script.js
@@ -0,0 +1,40 @@
+$(".come").click(() => {
+  alert("Belom tersedia lur");
+});
+
+// navbar start
+$(".open").click(() => {
+  $(".list-item").css({
+    transform: "translateX(0%)",
+  });
+});
+
+$(".close").click(() => {
+  $(".list-item").css({
+    transform: "translateX(100%)",
+  });
+});
+// navbar end
+
+// home start
+$(".contain").css({
+  scale: "100%",
+});
+// home end
+
+// card start
+$(window).scroll(function () {
+  $(".card").each(function () {
+    var imagePos = $(this).offset().top;
+    var topOfWindow = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (imagePos < topOfWindow + windowHeight && imagePos >= topOfWindow) {
+      $(this).css({ scale: "100%" });
+    }
+  });
+});
+// card end
+
+$(".controll-page").css({
+  zIndex: 10,
+});
